20370e39d8a7d01f8a607c28bb08e5c40c6ebc4d
Makoto Ishida <mike@ishida-it.com>
2015-10-16 12:45:32 -1000

    Add Browserify and Gulp to automate transformation.

---
+++ js/main.jsx
@@ -0,0 +1,40 @@
+// React をロード
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// 外部ファイルへ分割した Message クラスをロード
+var Message = require('./message.jsx');
+
+// このアプリケーションのメインとなる App クラス
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      person: {
+        name: 'ヤマダ',
+        age: 34
+      }
+    };
+  },
+  handleChange: function(event) {
+    this.setState({
+      person: {
+        name: event.target.value,
+        age: this.state.person.age
+      }
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="text" value={this.state.person.name} onChange={this.handleChange} />
+        <Message name={this.state.person.name} age={this.state.person.age} />
+      </div>
+    );
+  }
+});
+
+// app クラスを描画
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
