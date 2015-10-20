1c8c49f4396571bfc56d231a805bc45ebdedc682
Makoto Ishida <mike@ishida-it.com>
2015-10-20 10:19:37 -1000

    Adjust gitignore.

--- js/constants.jsx
+++ js/constants.jsx
@@ -2,6 +2,7 @@ module.exports = {
     ADD_ITEM: "add_item",
     EDIT_ITEM: "edit_item",
     REMOVE_ITEM: "remove_item",
+	TOGGLE_ITEM: "toggle_item",
 	CHECK_ITEM: "check_item",
 	UNCHECK_ITEM: "uncheck_item",
 	CHECK_ALL: "check_all",
